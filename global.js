0e0fa370e18f03a32ba3adcb17a860830ea71791
Eric <emei@ucsd.edu>
2026-04-25 13:38:01 -0700

    j

--- global.js
+++ global.js
@@ -86,7 +86,38 @@ export async function fetchJSON(url) {
   try {
     // Fetch the JSON file from the given URL
     const response = await fetch(url);
+    if (!response.ok) {
+  throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
+}
+
+// export function renderProjects(project, containerElement, headingLevel = 'h2') {
+//   containerElement.innerHTML = '';
+//   const article = document.createElement('article');
+//   article.innerHTML = `
+//     <h3>${project.title}</h3>
+//     <img src="${project.image}" alt="${project.title}">
+//     <p>${project.description}</p>
+// `;
+// containerElement.appendChild(article);
+// }
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  projects.forEach(project => {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  });
 }
