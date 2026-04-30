8c8086670112a6a37a7a58cef6b6a0eb509337fe
Eric <emei@ucsd.edu>
2026-04-30 16:47:35 -0700

    dd

--- global.js
+++ global.js
@@ -106,6 +106,23 @@ return data;
 // `;
 // containerElement.appendChild(article);
 // }
+
+// export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+//   containerElement.innerHTML = '';
+
+//   projects.forEach(project => {
+//     const article = document.createElement('article');
+
+//     article.innerHTML = `
+//       <${headingLevel}>${project.title}</${headingLevel}>
+//       <img src="${project.image}" alt="${project.title}">
+//       <p>${project.description}</p>
+//     `;
+
+//     containerElement.appendChild(article);
+//   });
+// }
+
 export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   containerElement.innerHTML = '';
 
@@ -115,7 +132,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      <div>
+        <p>${project.description}</p>
+        <p class="project-year">${project.year}</p>
+      </div>
     `;
 
     containerElement.appendChild(article);
