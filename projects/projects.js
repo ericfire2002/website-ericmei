1d8b6734d104165467d9bde04c0eb6b49266331b
Eric <emei@ucsd.edu>
2026-05-02 14:22:13 -0700

    d

--- projects/projects.js
+++ projects/projects.js
@@ -26,14 +26,15 @@ let arcGenerator = d3.arc()
 //   .attr('d', arc)
 //   .attr('fill', 'red');
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let sliceGenerator = d3.pie()
   .value(d => d.value);
