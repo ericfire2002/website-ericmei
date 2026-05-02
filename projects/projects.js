e1d9f1fb3d38f0513d0fee8b2e62c2b01b1b00bf
Eric <emei@ucsd.edu>
2026-05-02 13:22:56 -0700

    d

--- projects/projects.js
+++ projects/projects.js
@@ -21,7 +21,7 @@ let arc = arcGenerator({
   endAngle: 2 * Math.PI,
 });
 
-d3.select('#projects-plot')
+d3.select('#projects-pie-plot')
   .append('path')
   .attr('d', arc)
   .attr('fill', 'red');
