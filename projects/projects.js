f93e996ccd2c12ad09b5f8fb8527dc491082404f
Eric <emei@ucsd.edu>
2026-05-02 16:31:41 -0700

    d

--- projects/projects.js
+++ projects/projects.js
@@ -14,6 +14,53 @@ let query = '';
 
 projectsTitle.textContent = `Projects (${projects.length})`;
 
+function renderPieChart(projectsGiven) {
+  let rolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+
+  let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
+
+  let arcGenerator = d3.arc()
+    .innerRadius(0)
+    .outerRadius(50);
+
+  let sliceGenerator = d3.pie()
+    .value(d => d.value);
+
+  let arcData = sliceGenerator(data);
+  let arcs = arcData.map(d => arcGenerator(d));
+
+  let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+  let svg = d3.select('#projects-pie-plot');
+  svg.selectAll('path').remove();
+
+  let legend = d3.select('.legend');
+  legend.selectAll('li').remove();
+
+  arcs.forEach((arc, idx) => {
+    svg
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx));
+  });
+
+  data.forEach((d, idx) => {
+    legend
+      .append('li')
+      .attr('style', `--color:${colors(idx)}`)
+      .attr('class', 'legend-item')
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+  });
+}
+
+renderPieChart(projects);
+
 let searchInput = document.querySelector('.searchBar');
 
 searchInput.addEventListener('input', (event) => {
@@ -25,58 +72,7 @@ searchInput.addEventListener('input', (event) => {
   });
 
   renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
 
   projectsTitle.textContent = `Projects (${filteredProjects.length})`;
 });
-
-
-let arcGenerator = d3.arc()
-  .innerRadius(0)
-  .outerRadius(50);
-
-// let arc = arcGenerator({
-//   startAngle: 0,
-//   endAngle: 2 * Math.PI,
-// });
-
-// d3.select('#projects-pie-plot')
-//   .append('path')
-//   .attr('d', arc)
-//   .attr('fill', 'red');
-
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
-
-let data = rolledData.map(([year, count]) => {
-  return { value: count, label: year };
-});
-
-let sliceGenerator = d3.pie()
-  .value(d => d.value);
-
-let arcData = sliceGenerator(data);
-
-let arcs = arcData.map(d => arcGenerator(d));
-
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-arcs.forEach((arc, idx) => {
-  d3.select('#projects-pie-plot')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', colors(idx));
-});
-
-let legend = d3.select('.legend');
-
-data.forEach((d, idx) => {
-  legend
-    .append('li')
-    .attr('style', `--color:${colors(idx)}`)
-    .attr('class', 'legend-item')
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
-});
-
